2471bf8c6a67435fad5e8fcd568c156599ce715b
AlvinPaycards <alvin@payecards.com>
2023-09-28 08:18:18 +0300

    Initial Commit

---
+++ SwiftUiStarter/SwiftUiStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiStarterApp.swift
+//  SwiftUiStarter
+//
+//  Created by Alvin on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
